b11e59cce71018e5773caab29551d8bd19e03bfd
souad elma <sou.elmarsani@gmail.com>
2023-03-20 15:51:55 -0700

    this is c programme : negative or positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,20 +11,20 @@ int main(void)
 {
     int n;
     int last_digi;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
     last_digi = n%10;
     if(last_digi >5)
     {
-        printf("Last digital of %d is %d and is greater than 5" , n , last_digi);
+        	printf("Last digital of %d is %d and is greater than 5" , n , last_digi);
     }
     else if(last_digi==0)
         {
-            printf("Last digital of %d is %d and is 0" , n , last_digi);
+            	printf("Last digital of %d is %d and is 0" , n , last_digi);
         }
         else if(last_digi<6 && last_digi!=0)
             { 
-                printf("Last digital of %d is %d and is less than 6 and not 0" , n , last_digi);
+                	printf("Last digital of %d is %d and is less than 6 and not 0" , n , last_digi);
             }
-	return (0);
+    return (0);
 }
