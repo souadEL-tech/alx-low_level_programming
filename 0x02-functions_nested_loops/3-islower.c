05523941c828b428e71b2fa25fed6fa2b7336f65
souad elma <sou.elmarsani@gmail.com>
2023-03-21 18:19:34 -0700

    lowercase programe

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _islower - test if the caracter is lowercase of not
+* @c :  character to check
+* Return: 1 if c is lowercase , 0 if c is upsercase
+*/
+
+int _islower(int c);
+{
+int i;
+
+i = islower(c);
+if (i != 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
