e30986d63ab1343deae1b55fb5f90d299cf63b1e
souad elma <sou.elmarsani@gmail.com>
2023-03-21 17:04:39 -0700

    task 1 alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+ #include "main.h"
+/**
+ * print_alphabet - print all alphabet in lowcase
+ */
+void print_alphabet(void)
+{
+char letter = 'a';
+
+while (letter != '{')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
