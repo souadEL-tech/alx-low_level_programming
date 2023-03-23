3818e716e7909ba1f190f0c45647a003b910efc1
souad elma <sou.elmarsani@gmail.com>
2023-03-22 17:35:58 -0700

    all tasks

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,23 +1,17 @@
 #include "main.h"
+
 /**
- * _abs: print the absolut value of a number
- * @n: parameter to ckeck
- * Return: Always 0
+ * _abs - compute absolute value of integer
+ * @n: integer argument
+ * Return: absolute value
  */
+
 int _abs(int n)
 {
+	if (n > 0)
+		return (n);
+	else if (n < 0)
+		return (-n);
 
-if (n >= 0)
-{
-return (n);
-}
-else if (n < 0)
-{
-n = n*(-1);
-return (n);
-}
-
-_putchat('\n');
-return (0);
-
+	return (0);
 }
