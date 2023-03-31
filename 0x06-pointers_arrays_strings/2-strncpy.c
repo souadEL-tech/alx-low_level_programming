327feb27c65e3e450d524a1add8eff94a4b6194f
souad elma <sou.elmarsani@gmail.com>
2023-03-30 19:30:05 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strncpy - copy string
+ * @dest: pointer , parameter
+ * @src: pointer , parameter
+ * @n: parameter
+ * Return: char pointer
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+int j = 0;
+
+while (src[i] != '\0' && i < n)
+{
+dest[j] = src[i];
+j++;
+i++;
+}
+
+dest[i] = '\0';
+return (dest);
+}
