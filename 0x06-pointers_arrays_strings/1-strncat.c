ff06e6004023486c12854e592cffd1eeb4fc74b1
souad elma <sou.elmarsani@gmail.com>
2023-03-30 19:09:21 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strncat - concatenates two strings with limit byte
+ * @dest: pointer , parameter
+ * @src: pointer , parameter
+ * @n: parameter
+ * Return: char pointer
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0;
+int j = 0;
+
+while (dest[i] != '\0')
+{
+i++;
+}
+while (j <= n)
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+
+return (dest);
+}
