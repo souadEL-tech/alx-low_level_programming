c90a7f6fe79b02f189c64e9243e630ec863bc3f9
souad elma <sou.elmarsani@gmail.com>
2023-03-31 18:37:33 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,20 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i = 0;
-int j = 0;
-
-while (s1[i] != '\0' && s2[i] != '\0')
-{
-if (s1[i] == s2[i])
-{
-i++;
-}
-else
-{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
 	return (*s1 - *s2);
-}
-}
-
-return (0);
 }
