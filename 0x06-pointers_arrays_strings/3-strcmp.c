70dbc7d81b7950e389fee890331e07d61c04a97c
souad elma <sou.elmarsani@gmail.com>
2023-03-31 17:13:16 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *_strcmp - compare 2 string
+ * @s1: parameter 1
+ * @s2: parameter 2
+ * Return: return 0 if equale , 1 if s1 > s2 , -1 if s1 > s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+int j = 0;
+
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] == s2[i])
+{
+j++;
+}
+else if (s1[i] > s2[i])
+{
+return (1);
+}
+else
+{
+return (-1);
+}
+}
+
+return (0);
+}
