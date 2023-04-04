8f3b8d3a5be91a93aea5cdc06ca9c615822d452a
souad elma <sou.elmarsani@gmail.com>
2023-04-03 20:08:03 -0700

    char *_memcpy(char *dest, char *src, unsigned int n)

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_memcpy -copies memory area
+* @dest: parameter 1
+* @src: parameter 2
+* @n: unsigned int
+*Return: return pointer
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
