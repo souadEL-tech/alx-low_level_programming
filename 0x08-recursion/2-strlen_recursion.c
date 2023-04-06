9a40af805c47798db2f05ee388161852827be2e7
souad elma <sou.elmarsani@gmail.com>
2023-04-05 19:52:31 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion -string lenght recursive
+ * @s: parameter
+ * Return: return len of string
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		_strlen_recursion(s++);
+	}
+	return (i);
+}
