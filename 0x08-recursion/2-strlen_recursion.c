274ebec037a516c5a621db114534dc195b695a63
souad elma <sou.elmarsani@gmail.com>
2023-04-05 20:18:15 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,12 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
 
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		i++;
-		_strlen_recursion(s + 1);
+		return (0);
 	}
-	return (i);
+
+	return (1 +  _strlen_recursion(s++));
+
 }
