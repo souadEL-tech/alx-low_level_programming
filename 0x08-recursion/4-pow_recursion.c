6c41b21ed388b174e556b0454c3f0546a829eea4
souad elma <sou.elmarsani@gmail.com>
2023-04-06 18:33:03 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,14 +7,16 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y == 0)
-	{
-		return (0);
-	}
-	else if (y < 0)
+	int fact = 0;
+
+	if (y < 0)
 	{
 		return (-1);
 	}
-	return (x * _pow_recursion(x, y - 1));
+	else if (y > 0)
+	{
+		fact = x * _pow_recursion(x, y - 1);
+	}
+	return (fact);
 }
 
