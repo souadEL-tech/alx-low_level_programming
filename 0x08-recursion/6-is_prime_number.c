7f04584da6aff9cf377004a73b9d93b496fcdd78
souad elma <sou.elmarsani@gmail.com>
2023-04-06 19:46:29 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,13 +7,13 @@
 int is_prime_number(int n)
 {
 
-	if (n % 2 > 0)
+	if (n == 1)
+	{
+		return (0);
+	}
+	else if (n % 2 > 0)
 	{
 		return (1);
-	}
-	else if (n == 1)
-	{
-		return(0);
 	}
 	return (0);
 }
