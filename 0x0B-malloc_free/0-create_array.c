b77b181c7b455c47d237b857cbc3c9ab62fd5094
souad elma <sou.elmarsani@gmail.com>
2023-04-11 17:56:13 -0700

    0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array -create array with malloc
+ * @size: parameter 1
+ * @c: parameter 2
+ * Return: pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr = malloc(size * sizeof(char));
+	unsigned int i = 0;
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		ptr[i] = c;
+	}
+	return (ptr);
+}
