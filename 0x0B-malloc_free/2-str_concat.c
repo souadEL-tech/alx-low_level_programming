b04e61303f8420018ce8de1bffde067ca08064eb
souad elma <sou.elmarsani@gmail.com>
2023-04-12 17:30:39 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,39 +8,40 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	int len1 = 0;
+	int len2 = 0;
+	char *ptr;
+	int i = 0;
+	int j = 0;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
-
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s3 == NULL)
+	if (s1 == NULL && s2 == NULL)
+	{
 		return (NULL);
-	i = 0;
-	j = 0;
-
-	if (s1)
-	{
-		while (i < len1)
-		{
-			s3[i] = s1[i];
-			i++;
-		}
 	}
-
-	if (s2)
+	while (s1[len1] != '\0')
 	{
-		while (i < (len1 + len2))
-		{
-			s3[i] = s2[j];
-			i++;
-			j++;
-		}
+		len1++;
 	}
-	s3[i] = '\0';
-	return (NULL);
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= len1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	i = i + 1;
+	while (s2[j] != '\0')
+	{
+		ptr[i] = s2[j];
+		j++;
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
-
