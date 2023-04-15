052894d4613b8973b76da4379183832799ada204
souad elma <sou.elmarsani@gmail.com>
2023-04-14 19:24:56 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,38 +11,31 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
 	unsigned int len1 = 0, len2 = 0;
-	unsigned int i = 0;
-	unsigned int j = 0;
+	unsigned int i = 0, j = 0;
 
 	while (s1 && s1[len1])
-	{
 		len1++;
-	}
-
 	while (s2 && s2[len2])
-	{
 		len2++;
-	}
 	if (n < len2)
 		ptr = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		ptr = malloc(sizeof(char) * (len1 + len2) + 1);
+		ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (ptr == NULL)
 		return (NULL);
-	while (s1[i] != '\0')
+	while (i < len1)
 	{
 		ptr[i] = s1[i];
 		i++;
 	}
+
 	while (n < len2 && i < (len1 + n))
-	{
 		ptr[i++] = s2[j++];
-	}
+
 	while (n >= len2 && i < (len1 + len2))
-	{
 		ptr[i++] = s2[j++];
-	}
+
 	ptr[i] = '\0';
 	return (ptr);
 }
