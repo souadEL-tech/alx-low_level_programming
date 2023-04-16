3478f4552d3b59c771809a3503231bf893d22f34
souad elma <sou.elmarsani@gmail.com>
2023-04-15 19:57:43 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -function
+ * @argc: parameter
+ * @argv: parameter
+ *
+ * Return: return int val
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
