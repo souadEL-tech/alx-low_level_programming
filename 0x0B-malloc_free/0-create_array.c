cd1dc18b0fbbd727da35dabc3cbe48dac8db8240
souad.el <sou.elmarsani@gmail.com>
2023-06-12 16:49:53 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,14 +7,12 @@
  * @c: char
  * Return: return ptr
  */
-char * create_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
 	char *ptr;
 
 	ptr = (char *) malloc(sizeof(char) * size);
-	if (size == 0)
-		return (0);
 	if (ptr == NULL)
 	{
 		return (0);
