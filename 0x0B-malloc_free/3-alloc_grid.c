5d8d485bc92781276ed0e151cc0bf2010ce8bc2a
souad.el <sou.elmarsani@gmail.com>
2023-06-14 13:21:17 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,6 +9,21 @@
 
 int **alloc_grid(int width, int height)
 {
-	int *ptrl;
-	int l=0, C=0;
+	int **ptr;
+	int l = 0, c = 0;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	ptr = (int **) malloc(sizeof(int) * (width * height));
+	if (ptr == NULL)
+		return (NULL);
+	for (l = 0; l < width; l++)
+	{
+		for (c = 0; c < height; c++)
+		{
+			*(ptr + l + c) = 0;
+		}
+	}
+
+	return (ptr);
 }
