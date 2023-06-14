b8ab565234321d023b4f07029b32f8b5539c66b5
souad.el <sou.elmarsani@gmail.com>
2023-06-14 05:12:47 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,33 +1,34 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * *str_concat -concate 2 string
  * @s1: string 1
  * @s2: string 2
  * Return: return ptr
  */
-
-
 char *str_concat(char *s1, char *s2)
 {
-	char *ptr, c = ' ';
-	int size_s1 = 0, size_s2 = 0;
-	int str1 = 0, str2 = 0;
+	char *ptr;
+	int size_s1 = 0, size_s2 = 0, str1 = 0, str2 = 0, sizefinale = 0;
 
-	while (*(s1 + size_s1))
-		size_s1++;
-
-	while (*(s2 + size_s2))
-		size_s2++;
-
-	ptr = (char *) malloc(sizeof(char) * (size_s1 + size_s2 + 1));
+	if (s1)
+	{
+		while (*(s1 + size_s1))
+			size_s1++;
+	}
+	else
+		size_s1 = size_s1 + 1;
+	if (s2)
+	{
+		while (*(s2 + size_s2))
+			size_s2++;
+	}
+	else
+		size_s2 = size_s2 + 2;
+	sizefinale = (size_s1 + size_s2 + 1);
+	ptr = (char *) malloc(sizeof(char) * sizefinale);
 	if (ptr == NULL)
 		return (NULL);
-
-	if (s1 == NULL)
-		ptr[str1] = c;
-
 	if (s1)
 	{
 		while (str1 < size_s1)
@@ -36,10 +37,6 @@ char *str_concat(char *s1, char *s2)
 			str1++;
 		}
 	}
-
-	if (s2 == NULL)
-		ptr[str1] = c;
-
 	if (s2)
 	{
 		while (*(s2 + str2))
