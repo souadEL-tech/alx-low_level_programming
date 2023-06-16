6380cea6cba4221914147a4aa09b71755e4a1d4f
souad.el <sou.elmarsani@gmail.com>
2023-06-16 12:45:58 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,54 +1,57 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * *string_nconcat -concate 2 string
- * @s1: string 1
- * @s2: string 2
- * @n: int parameter
+ * *string_nconcat -function that concatenates two strings.
+ * @s1: string1
+ * @s2: string2
+ * @n: number of caracter to concat
  * Return: return ptr
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int count = 0, count1 = 0, i = 0, j = 0;
-	int sign = n;
 	char *ptr;
-	int len1 = 0, len2 = 0;
+	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
 
 	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	while (s1[count])
-		len1++;
-
-	while (s2[count1])
-		len2++;
-
-	if (sign >= len2)
 	{
+		s1 = "";
+		len1 = 1; }
+	else
+	{
+		while (s1[len1] != '\0')
+			len1++; }
+	if (s2 == NULL)
+	{
+		s2 = "";
+		len2 = 1; }
+	else
+	{
+		while (s2[len2] != '\0')
+			len2++; }
+	if (n >= len2)
 		ptr = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
-		sign = len2;
-	}
-
 	else
 		ptr = (char *) malloc(sizeof(char) * (len1 + n + 1));
-
-	while (s1[i])
+	if (ptr == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
 	{
 		ptr[i] = s1[i];
-		i++;
-	}
-
-	while (j < sign)
+		i++; }
+	if (n >= len2)
 	{
-		ptr[i] = s2[j];
-		i++;
-		j++;
-	}
-
+		while (s2[j] != '\0')
+		{
+			ptr[i] = s2[j];
+			i++;
+			j++; }}
+	else
+	{
+		while (j < n)
+		{
+			ptr[i] = s2[j];
+			i++;
+			j++; } }
 	ptr[i] = '\0';
 	return (ptr);
 }
