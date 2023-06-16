614862a9cfa5479c8074e6d6d4496a84d414871d
souad.el <sou.elmarsani@gmail.com>
2023-06-16 13:09:49 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,6 +28,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		ptr = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
 	else
 		ptr = (char *) malloc(sizeof(char) * (len1 + n + 1));
+	if (ptr == NULL)
+		return (NULL);
 	while (s1[i] != '\0')
 	{
 		ptr[i] = s1[i];
