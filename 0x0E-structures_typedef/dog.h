3ea5bc61090eee26e3ee936284ef538b079fc389
souad.el <sou.elmarsani@gmail.com>
2023-06-20 04:09:02 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+
+/**
+ * struct dog -structur Poppy
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
