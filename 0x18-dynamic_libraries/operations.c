72d6725748ae43d3e9d7e6fb0f8e52378241a87d
SOUAD_TECH <sou.elmarsani@gmail.com>
2023-12-11 13:42:58 -0800

    100-operations.so

--- 0x18-dynamic_libraries/operations.c
+++ 0x18-dynamic_libraries/operations.c
@@ -1,1 +1,54 @@
-#include "main.h"
+
+/**
+ * add -addition function
+ * @a: parameter
+ * @b: parameter
+ * Return: return int val
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub -function
+ * @n1: parameter
+ * @n2: parametr 2
+ * Return: return int function
+ */
+
+int sub(int n1, int n2)
+{
+	return (n1 - n2);
+}
+/**
+ * mul -function
+ * @num1: parametter
+ * @num2: parametter
+ * Retunr: int
+ */
+
+
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
+
+/**
+ * div -function
+ * @number1: parameter
+ * @number2: parameter
+ * Return: return int val
+ */
+
+int div(int number1, int number2)
+{
+	return (number1 / number2);
+}
+
+int mod(int i, int j)
+{
+	return (i % j);
+}
+
+
